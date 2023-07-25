fb7992b2bba8556c6b02afe35843a43fb168c1bb
raubertldlc <r.aubert@ldlc.com>
2023-07-25 22:55:57 +0200

    improve structure

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #61443d;
   margin-top: 60px;
+}
+html {
+  background: #aaaaaa;
 }
 </style>
